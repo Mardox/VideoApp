a3d8d63786574b26f0e20c58e17618a8440c903a
HooMan <hooman@wpdevclass.com>
2014-03-21 08:07:29 +1100

    Renaming Class Imports

--- app/src/main/java/com/thirtydaylabs/videoapp/app/PushActivity.java
+++ app/src/main/java/com/thirtydaylabs/videoapp/app/PushActivity.java
@@ -28,7 +28,7 @@ import com.google.android.youtube.player.YouTubeBaseActivity;
 import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
 import com.google.android.youtube.player.YouTubePlayerView;
-import com.thirtydaylabs.pksongs.R;
+import com.thirtydaylabs.nigerianmovies.R;
 import com.thirtydaylabs.videoapp.utilities.MenuFunctions;
 
 /**
