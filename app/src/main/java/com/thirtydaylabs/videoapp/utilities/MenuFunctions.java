a1bcb0524205eef543ebc6c518bbc1cb9f2fdf2b
HooMan <hooman@wpdevclass.com>
2014-03-23 09:35:12 +1100

    string and package set

--- app/src/main/java/com/thirtydaylabs/videoapp/utilities/MenuFunctions.java
+++ app/src/main/java/com/thirtydaylabs/videoapp/utilities/MenuFunctions.java
@@ -9,7 +9,7 @@ import android.content.SharedPreferences;
 import android.net.Uri;
 import android.widget.Toast;
 
-import com.thirtydaylabs.pksongs.R;
+import com.thirtydaylabs.snookertipsandtricks.R;
 import com.thirtydaylabs.videoapp.app.AboutActivity;
 import com.thirtydaylabs.videoapp.app.CollectionActivity;
 import com.thirtydaylabs.videoapp.app.PushActivity;
